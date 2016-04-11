7e037a903ad3a1ffc5f27b88332e32f713be8a3e
sharonrussell <sharon.russell@comparethemarket.com>
2016-04-11 20:09:57 +0100

    Add a test which fails for number divisible by 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,6 +7,10 @@ describe FizzBuzz do
 	end
 
 	it "returns fizz when number is 3" do
-           expect(@fizzbuzz.get(3)).to eq("Fizz")
+        expect(@fizzbuzz.get(3)).to eq("Fizz")
     end
+
+	it "returns buzz when number is 5" do
+		expect(@fizzbuzz.get(5)).to eq("Buzz")
+	end
 end
