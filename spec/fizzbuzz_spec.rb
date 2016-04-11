db9cf9143810d5ff08c38f26004baab86b3b97b5
sharonrussell <sharon.russell@comparethemarket.com>
2016-04-11 20:17:12 +0100

    Add test for number divisible by 5 and 3

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,11 +6,15 @@ describe FizzBuzz do
 	    @fizzbuzz = FizzBuzz.new
 	end
 
-	it "returns fizz when number is 3" do
+	it "returns fizz when number is divisible 3" do
         expect(@fizzbuzz.get(3)).to eq("Fizz")
     end
 
-	it "returns buzz when number is 5" do
+	it "returns buzz when number is divisible 5" do
 		expect(@fizzbuzz.get(5)).to eq("Buzz")
+	end
+
+	it "returns fizzbuzz when number is both divisible by 5 and 3" do
+		expect(@fizzbuzz.get(15)).to eq("FizzBuzz")
 	end
 end
