20ed7b3cb43634373a4e3642225f61ca0d883442
sharonrussell <sharon.russell@comparethemarket.com>
2016-04-11 20:17:24 +0100

    Add code to make test pass for number divisible by 5 and 3

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,13 @@
 class FizzBuzz
 	def get(number)
+		if(number % 15 === 0)
+			return "FizzBuzz"
+		end
 		if(number % 3 == 0)
 			return "Fizz"
 		end
 		if(number % 5 == 0)
 			return "Buzz"
-		end	
+		end
 	end
 end
