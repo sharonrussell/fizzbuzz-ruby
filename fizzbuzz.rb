0e5a17fdaa583ed162d0088cb6a7c5f03bfe45cc
sharonrussell <sharon.russell@comparethemarket.com>
2016-12-17 21:32:04 +0000

    refactor

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,18 +1,10 @@
 class FizzBuzz
 	def get(number)
-		if(number == 0)
-			return number
-		end
-		if(number % 15 === 0)
-			return "FizzBuzz"
-		end
-		if(number % 3 == 0)
-			return "Fizz"
-		end
-		if(number % 5 == 0)
-			return "Buzz"
-		end
 
-		return number
+		return number if number == 0
+		return "FizzBuzz" if number % 15 == 0
+		return "Fizz" if number % 3 == 0
+		return "Buzz" if number % 5 == 0
+		number
 	end
 end
